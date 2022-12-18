f8dc5f9442e2a293d9db656b7f3f3824e4d18fef
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2022-12-18 10:35:19 +0100

    Add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "cta-image": "url(../public/cta_image.png)",
+        "cta-image2": "url(../public/cta_image_2.png)",
+        "cta-image3": "url(../public/cta_image_3.png)",
+      },
       colors: {
         primary: "#ed7f39",
       },
